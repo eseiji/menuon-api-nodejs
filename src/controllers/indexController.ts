02c73ad3653ff788ec9881ec431a7b7d1de05d9d
Enzo Seiji <enzoseiji14@gmail.com>
2022-10-26 21:48:50 -0300

    date adjustments

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -132,7 +132,7 @@ export const insertOrder = async (req: Request, res: Response) => {
     let date = new Date();
     let local_date = date.toLocaleDateString("pt-BR");
     let local_time = date.toLocaleTimeString("pt-BR");
-    let insertion_date = `${local_date}T${local_time}`;
+    let insertion_date = `${local_date}T${local_time}` as String;
 
     let newOrder = await Order.create({
       total,
